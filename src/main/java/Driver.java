852c42eb87e270f91281d72ae2d1d80b81798dc0
Fiffah <itsmefiffah@gmail.com>
2023-05-16 22:57:03 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,15 +6,15 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Current Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.setCount());
 		
 		counter.increment();
 		
-		System.out.println("Current Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.setCount());
 		
 		counter.decrement();
 		
-		System.out.println("Current Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.setCount());
 		
 	}
 
